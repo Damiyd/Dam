a44b96e313b319e30fdcd45cc5062d0b390900d5
Damiyd <112368737+Damiyd@users.noreply.github.com>
2022-10-05 17:52:44 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/post")
+    .connect("mongodb://127.0.0.1/post")
     .catch(err => console.log(err));
 };
 
